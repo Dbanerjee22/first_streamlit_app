fbb31d5aef81824cb2b6574354d26ac364efec64
Dwaipayan Banerjee <54280282+Dbanerjee22@users.noreply.github.com>
2022-12-07 12:31:10 +0530

    Update streamlit_app.py
    copying streamlit details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
-streamlit.title('This is me trying new things')
-streamlit.text('just testing,having fun')
+sstreamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
