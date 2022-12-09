7aaa5e1295eb9214f191db965759503b02602aa5
Dwaipayan Banerjee <54280282+Dbanerjee22@users.noreply.github.com>
2022-12-09 19:44:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ if fruits_selected:
 else:
   streamlit.dataframe(my_fruit_list)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
 # converting json data into flat table 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
